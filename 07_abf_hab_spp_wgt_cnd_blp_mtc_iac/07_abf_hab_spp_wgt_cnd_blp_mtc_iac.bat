2133175dc529a32574e6dac05cb273868f0b76d4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-07-18 15:42:31 +0300

    Update 07 bat

--- 07_abf_hab_spp_wgt_cnd_blp_mtc_iac/07_abf_hab_spp_wgt_cnd_blp_mtc_iac.bat
+++ 07_abf_hab_spp_wgt_cnd_blp_mtc_iac/07_abf_hab_spp_wgt_cnd_blp_mtc_iac.bat
@@ -1,1 +1,1 @@
-call zig4.exe -r UML_07_MSP_mcki_ESS_L_abf_HM1_in/settings.dat UML_07_MSP_mcki_ESS_L_abf_HM1_in/features_list.spp UML_07_MSP_mcki_ESS_L_abf_HM1_out/UML_07_MSP_mcki_ESS_L_abf_HM1.txt 0.0 0 1.0 0  --grid-output-formats compressed-tif
+call zig4.exe -r 07_abf_hab_spp_wgt_cnd_blp_mtc_iac.dat 07_abf_hab_spp_wgt_cnd_blp_mtc_iac.spp 07_abf_hab_spp_wgt_cnd_blp_mtc_iac_out/07_abf_hab_spp_wgt_cnd_blp_mtc_iac.txt 0.0 0 1.0 0  --grid-output-formats compressed-tif
